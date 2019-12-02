96491b054542bb1967e90c12baaa8b8f06a639d0
Chetan Tayal <chetantayal138@gmail.com>
2019-12-02 18:05:33 +0530

    Added backprop with SGD

--- Neural Network/NN.py
+++ Neural Network/NN.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from utils import load_matlab,sigmoid
 from scipy.io import loadmat
 
-alpha = 0.001
+
 weights = loadmat('ex3weights.mat')
 theta1,theta2 = weights['Theta1'],weights['Theta2']
 theta2 = np.roll(theta2 , 1, axis=0)
@@ -40,8 +40,9 @@ class Layer:
         self.x = input_values
 
     def init_weights(self,num_nodes_prev,weights):
-        if(type(weights) == "<class 'int'>"):
-            self.weights = np.random.randn(num_nodes_prev,self.num_nodes)
+
+        if(weights ==0):
+            self.weights = np.random.randn(num_nodes_prev,self.num_nodes)*np.sqrt(2/num_nodes_prev)
 
         else:
             self.weights = weights.T
@@ -60,7 +61,7 @@ X,y = load_matlab('ex3data1.mat')
 
 
 
-k=0
+"""k=0
 for i in range(X.shape[0]):
     L1 = Layer(400,"sigmoid","INPUT")
     L1.init_x(X[i,:])
@@ -81,46 +82,55 @@ for i in range(X.shape[0]):
     k = k + x
    
 
-print(f'Training Set Accuracy: {(k/X.shape[0])*100}')
+print(f'Training Set Accuracy: {(k/X.shape[0])*100}')"""
 
 
 
-"""alpha = 0.1
-x=0
+alpha = 0.3
+
 J = []
 
-for epoch in range(1000):
+
+
+
+L1 = Layer(400,"sigmoid","INPUT")
+L2 = Layer(25 ,"sigmoid","HIDDEN")
+L2.init_weights(401,0)
+L3 = Layer(10 ,"sigmoid","OUTPUT")
+L3.init_weights(26,0)
+
+m = X.shape[0]
+
+
+for epoch in range(500):
     print(f"EPOCH IS {epoch+1}")
     
-    for i in range(X.shape[0]):
-        L1 = Layer(400,"sigmoid","INPUT")
-        L1.init_x(X[i,:])
+    k=0
+    for i in range(m):
+
     
-        L2 = Layer(25 , "sigmoid", "HIDDEN")
+        L1.init_x(X[i,:])    
         L2.init_x(L1.x,insert=True)
-        L2.init_weights(401,0)
-    
         L2.compute_z()
         L2.compute_a()
-        L3 = Layer(10, "sigmoid", "OUTPUT")
         L3.init_x(L2.a,insert=True)
-        L3.init_weights(26,0)
-    
         L3.compute_z()
-        L3.compute_a()
-        
+        t = np.exp(L3.z)
+        a = t/t.sum()
+        L3.a = a 
+        actuals = np.zeros((10))
         p =  np.argmax(L3.a)
         actual = y[i]
-        x = x + np.mean(p == y[i])
+        actuals[actual] = 1 
+    
+
+
+        x = np.mean(p == y[i])
+        k = k+x
+    
+                    
         
-        
-    print("TRAINING SET ACCURACY {:.1f}%".format(np.mean(p == y[i])))
-    
-    
-#if(i%100 == 0):
-         #   J.append( y[i] * np.log(L3.a) + (1-y[i]) * (1-np.log(L3.a)))
-    
-        L3.dz = L3.a - y[i]
+        L3.dz = L3.a -actuals    
         L3.db = np.sum(L3.dz)
     
         L3.dz = L3.dz.reshape(L3.dz.shape[0],-1)
@@ -128,19 +138,30 @@ for epoch in range(1000):
         L3.dw = np.dot(L3.dz, L2.a.T)
     
         g_prime = (sigmoid(L2.z) * (1-sigmoid(L2.z))).sum()
+    
         L2.dz = np.dot(L3.weights, L3.dz) * g_prime
         L1.x = L1.x.reshape(L1.x.shape[0],-1)     
         L2.dw = np.dot(L2.dz,L1.x.T)
-    
         L2.db = np.sum(L3.dz)
     
     
         L3.dw = np.insert(L3.dw,1,np.array(L3.db),axis = 1)
         L2.dw = np.insert(L2.dw,1,np.array(L2.db),axis = 1)
-    
-        L3.weights = L3.weights - alpha * L3.dw.T
-        L2.weights = L2.weights - alpha * L2.dw[:-1,:].T
-        if(i==X.shape[0]-1):
-            print(f"ACCURACY IS {(x/X.shape[0])*100}")"""
+
+        
+        L3.weights = L3.weights - ((alpha/m) * L3.dw.T)
+        L2.weights = L2.weights - ((alpha/m) * L2.dw[:-1,:].T)
+        
+        
+
+    if(epoch%50==0):
+        print(f"ACCURACY IS {(k/X.shape[0]) * 100}")
+
+
+
+
+
+
+
 
 
